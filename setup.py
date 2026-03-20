43ed18c83e52dcb07c37845066fac473e8cd4903
lq54afgthgf <6791036@stud.kai.edu.ua>
2026-03-20 10:58:18 +0200

    Виправлено розташування коду для тестів

--- setup.py
+++
@@ -1,5 +0,0 @@
-def hello_world():
-    print( "Hello World" )  # Навмисно зайві пробіли для перевірки black
-    
-def add_numbers(a,b): # Навмисно немає пробілу після коми для flake8
-    return a+b
